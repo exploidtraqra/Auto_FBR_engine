0fe80da9c3080da72cce02e6eda870b2f2f45cec
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-06-29 03:07:09 +0000

    Update FBR.sh

--- FBR.sh
+++ FBR.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "___ENGINEENGINEENGINEENG___"
 echo "FACEBOOK ENGINE BRUTEFORCE RANDOME"
+echo "1.scane id'
+echo '2.run'
 read -p pilih
+if [pilih == "1"]
+#then
+#else
+ #echo $pilih,"
